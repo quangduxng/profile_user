f79c676bfd45dd9f8b359d03db54a1223ad4bd30
QuangDung <dung05042000@gmail.com>
2022-01-13 21:56:54 +0700

    bosung

---
+++ func.js
@@ -0,0 +1,11 @@
+function bigImg(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+    x.style.transition ="1s"
+  }
+  
+  function normalImg(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+    x.style.trandition ="2s"
+  }
